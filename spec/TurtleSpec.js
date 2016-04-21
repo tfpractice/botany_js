32e146555a71f53790f01d727035180b27c76691
theory friction practice <jackson.brebnor@gmail.com>
2016-04-20 20:28:28 -0400

    tests Turtle#forward

--- spec/TurtleSpec.js
+++ spec/TurtleSpec.js
@@ -7,5 +7,17 @@ describe('Turtle', () => {
         it('has a heading', function() {
             expect(myTurtle.heading).toBeNumber();
         });
+        it('has a step attribute', function() {
+            expect(myTurtle.step).toBeNumber();
+        });
+        it('has a delta attr', function() {
+            expect(myTurtle.delta).toBeNumber();
+        });
+    });
+    describe('#forward', () => {
+        it('changes the position of the turtle by sin and cos of delta', function() {
+            myTurtle.forward();
+            expect(myTurtle.position.y).not.toBe(15);
+        });
     });
 });
--- src/Turtle.js
+++ src/Turtle.js
@@ -1,7 +1,20 @@
-function Turtle(x = 0, y = 0, head = 0) {
+function Turtle(x = 0, y = 0, head = 0, step = 5, delta = Math.PI / 2) {
     this.position = {
         x: x,
         y: y
     };
     this.heading = head;
+    this.step = step;
+    this.delta = delta;
 }
+
+Turtle.prototype.forward = function(draw = true) {
+    var dx = Math.cos(this.delta) * this.step;
+    var dy = Math.sin(this.delta) * this.step;
+    console.log(this.position);
+
+    this.position.x += dx;
+    this.position.y += dy;
+    console.log(this.position);
+
+};

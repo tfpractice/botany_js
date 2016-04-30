0a183c7635085521ecf948fedf28067ad311926a
theory friction practice <jackson.brebnor@gmail.com>
2016-04-30 16:53:07 -0400

    Tries nodeReplacement serpienski

--- src/LSystem.js
+++ src/LSystem.js
@@ -15,11 +15,21 @@ function setup() {
     qFlake.addVocab('F', 'F', 'F,+,F,-,F,-,F,+,F');
     var chain = new TSystem(400, Math.PI / 2, 0.25, 'F,-,F,-,F,-,F,-');
     chain.addVocab('F', 'F', 'F,F,-,F,-,F,-,F,-,F,-,F,+,F');
+
+    var serpienski = new TSystem(100, Math.PI / 3, 0.33, 'R');
+    serpienski.addVocab('R', 'F', '-,L,+,R,+,L');
+    serpienski.addVocab('L', 'F', '+,R,-,L,-,R');
     myTurtle = new Turtle(windowWidth / 8, windowHeight / 8, 0, island);
+
+
+
     var flakeT = new Turtle(windowWidth / 2, windowHeight / 2, 0, qFlake);
     var chainT = new Turtle(windowWidth / 8, windowHeight / 2, 0, chain);
+    var serpT = new Turtle(windowWidth / 8, windowHeight / 2, 0, serpienski);
     // console.log(myTurtle);
     // myTurtle.interpret();
+    // serpT.interpret();
+    // serpT.spawn(3);
     // myTurtle.spawn(3);
     // flakeT.spawn(3);
     // chainT.spawn(3);
